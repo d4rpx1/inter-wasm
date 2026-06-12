ce974f2e974915db427f97412970045a9c2f513e
d4rpx1 <pavelescu_darius@yahoo.com>
2026-06-12 17:30:24 +0200

    update Main

--- src/main/java/com/group4/interwasm/Main.java
+++ src/main/java/com/group4/interwasm/Main.java
@@ -3,10 +3,8 @@ package com.group4.interwasm;
 public class Main {
 
     static void main() {
+        // TODO: First use WasmReader class to read in a .wasm file as FunctionDef class
+        // then use Interpreter class to execute the FunctionDef
         IO.println(String.format("Hello and welcome!"));
-
-        for (int i = 1; i <= 5; i++) {
-            IO.println("i = " + i);
-        }
     }
 }
